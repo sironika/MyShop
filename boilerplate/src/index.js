d723dfc6bfd045d50e6949c2a5418dabf0204195
Sergiy Kolisnichenko <s.a.kolisnichenko@gmail.com>
2018-01-15 10:27:01 +0200

    +category to sidebar

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,35 +1,34 @@
 import './main.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import {browserHistory} from 'react-router'
+import thunk from 'redux-thunk'
 import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
-import reducers from 'reducers'
-import {Router, Route} from 'react-router'
 
+import reducers from 'reducers'
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
 import Phone from 'containers/phone'
 
 const store = createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunk)
+  applyMiddleware(thunk)
 ))
 
 const history = syncHistoryWithStore(browserHistory, store)
 
-
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Route component={Layout}>
-                <Route path='/' component={Phones} />
-            </Route>
-            <Route path='phones/:id' component={Phone} />
-            <Route path='categories/:id' component={Phones} />
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+        <Route path='/' component={Phones}/>
+        <Route path='/categories/:id' component={Phones}/>
+      </Route>
+      <Route path='/phones/:id' component={Phone}/>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
